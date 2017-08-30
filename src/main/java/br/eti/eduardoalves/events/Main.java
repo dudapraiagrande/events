2fdfa3d5d0e8dedc691994600d6bb9388dc24b13
Eduardo Alves da Silva <dudapraiagrande@gmail.com>
2017-08-30 01:00:25 -0300

    Enterprise application.

---
+++ src/main/java/br/eti/eduardoalves/events/Main.java
@@ -0,0 +1,12 @@
+package br.eti.eduardoalves.events;
+
+import br.eti.eduardoalves.events.controller.Controller;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Controller controller = new Controller();
+		controller.bussinessLogic();
+	}
+
+}
---
+++ src/main/java/br/eti/eduardoalves/events/controller/Controller.java
@@ -0,0 +1,11 @@
+package br.eti.eduardoalves.events.controller;
+
+public class Controller {
+	
+	public void bussinessLogic() {
+		System.out.println("Process 1!");
+		System.out.println("Process 2!");
+		System.out.println("Process 3!");
+	}
+
+}
